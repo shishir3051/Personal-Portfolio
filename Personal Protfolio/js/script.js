f6a709503314ad4f32cb8838b80d4b01d79ca6fa
NightMare <nightmare@localhost.localdomain>
2023-10-24 10:36:06 +0600

    Design complete

---
+++ Personal Protfolio/js/script.js
@@ -0,0 +1,19 @@
+var typed = new Typed(".typing",{
+    strings:["Software Engineer","Penetration Tester","Web Designer","Programmer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+const nav = document.querySelector(".nav"),
+        navList = nav.querySelectorAll("li"),
+        totalNavList = navList.length;
+        for(let i=0;i,totalNavList; i++){
+            const a = navList[i].querySelector("a");
+            a.addEventListener("click",function(){
+                for(let j=0; j<totalNavList;j++){
+                    navList[j].querySelector("a").classList.remove("active")
+                }
+                this.classList.add("active")
+            })
+        }
